fe2df586030cab8a8fd110d6f154c9ed43dafc9c
Andres Hung <andres.hung@outlook.com>
2023-10-11 21:59:18 -0600

    chore: cleared Next.js template

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Fundamentals of Computing',
+  description: 'Final project for Univserity of Belize Fundamentals of Computing course [CMPS1134-2]',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
