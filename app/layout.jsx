fbd8a6d9e55055084fa9620f7230603e4d00fe0c
Andres Hung <andres.hung@outlook.com>
2023-10-12 13:49:25 -0600

    chore: formatter change

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,18 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Fundamentals of Computing',
-  description: 'Final project for Univserity of Belize Fundamentals of Computing course [CMPS1134-2]',
-}
+  title: "Fundamentals of Computing",
+  description:
+    "Final project for Univserity of Belize Fundamentals of Computing course [CMPS1134-2]",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
