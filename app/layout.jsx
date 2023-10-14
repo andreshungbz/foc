7c31ef10e0d76bf9612abe521fe77faf9afa1611
Andres Hung <andres.hung@outlook.com>
2023-10-13 19:05:38 -0600

    feat: completed basic footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.jsx
@@ -0,0 +1,24 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footersection">
+        <p>Website and Design by Andres Hung</p>
+        <p>[CMPS1134-2] FOC Final Project</p>
+        <p>University of Belize</p>
+        <p>Semester 2023-1</p>
+      </div>
+      <div className="footersection">
+        <p>
+          <a href="https://github.com/andreshungbz/foc2023" target="_blank">
+            Project Source Code
+          </a>
+        </p>
+        <p>
+          <a href="https://github.com/andreshungbz" target="_blank">
+            GitHub Profile
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
