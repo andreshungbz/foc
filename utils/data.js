4965519c7b49b7bdc041e43166fafaed1d9cfd7f
Andres Hung <andres.hung@outlook.com>
2023-10-19 16:41:47 -0600

    feat: added data

--- utils/data.js
+++ utils/data.js
@@ -7,6 +7,40 @@ const concepts = [
     image: "/concept/ah-semaphores.png",
     link: 'https://docs.google.com/presentation/d/1T07PFXmvSNUeJN7coH5SmyqNuF5qZQJJp8kjrfHIims/edit?usp=share_link',
   },
+  {
+    name: "Jennessa Sierra",
+    title: "Internet Applications",
+    description: "Internet Applications are applications that run on the internet.",
+    image: "/concept/js-internet-applications.png",
+    link: "https://docs.google.com/presentation/d/10RCNFoBkhy49K3KyMioW_9bEd2450ayQM8dH3pMtFt0/edit?usp=sharing"
+  }
 ];
 
-export {concepts};
+const profiles = [
+  {
+    name: "Andres Hung",
+    biography: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mauris lacus, pellentesque at ex viverra, elementum condimentum lorem. Nunc sed ex vitae elit fermentum porta vel at sem. Curabitur eu rutrum arcu. Etiam in feugiat dui. Pellentesque mattis turpis nisl, a aliquet mauris tristique sed. Sed magna nisl, sagittis non augue euismod, facilisis bibendum libero. Quisque semper, ex et placerat pulvinar, metus diam iaculis turpis, et placerat ligula erat in arcu.",
+    image: "https://avatars.githubusercontent.com/u/115679125?v=4",
+    link: "https://github.com/andreshungbz",
+  },
+  {
+    name: "Jennessa Sierra",
+    biography: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mauris lacus, pellentesque at ex viverra, elementum condimentum lorem. Nunc sed ex vitae elit fermentum porta vel at sem. Curabitur eu rutrum arcu. Etiam in feugiat dui. Pellentesque mattis turpis nisl, a aliquet mauris tristique sed. Sed magna nisl, sagittis non augue euismod, facilisis bibendum libero. Quisque semper, ex et placerat pulvinar, metus diam iaculis turpis, et placerat ligula erat in arcu.",
+    image: "https://avatars.githubusercontent.com/u/143227481?v=4",
+    link: "https://github.com/jennxsierra",
+  },
+  {
+    name: "Placeholder",
+    biography: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mauris lacus, pellentesque at ex viverra, elementum condimentum lorem. Nunc sed ex vitae elit fermentum porta vel at sem. Curabitur eu rutrum arcu. Etiam in feugiat dui. Pellentesque mattis turpis nisl, a aliquet mauris tristique sed. Sed magna nisl, sagittis non augue euismod, facilisis bibendum libero. Quisque semper, ex et placerat pulvinar, metus diam iaculis turpis, et placerat ligula erat in arcu.",
+    image: "/profile/warwick.png",
+    link: "https://github.com/andreshungbz",
+  },
+  {
+    name: "Placeholder",
+    biography: "Hello",
+    image: "/profile/warwick.png",
+    link: "https://github.com/andreshungbz",
+  }
+];
+
+export {concepts, profiles};
