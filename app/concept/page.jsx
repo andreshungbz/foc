44f0a537c7536bf3c2b91a9186d77e633f73497b
Andres Hung <andres.hung@outlook.com>
2023-10-19 13:27:26 -0600

    feat: created basic concept page

--- app/concept/page.jsx
+++ app/concept/page.jsx
@@ -1,7 +1,21 @@
+import { concepts } from "@/utils/data.js";
+import ConceptItem from "@/components/ConceptItem";
+
 export default function Concept() {
   return (
     <div>
       <h1>Concept</h1>
+      <p>
+        Throughout the course of the Fundamentals of Computing course, students
+        are required to present on a particular section in the textbook,
+        Computer Science: An Overview (11th edition). This assessment is
+        intended for the student to improve their presentation skills while
+        learning about required concepts. Below are the presentations for our
+        dev team&apos;s concepts for the 2023-1 semester.
+      </p>
+      {concepts.map((concept) => {
+        return <ConceptItem key={concept.name} {...concept} />;
+      })}
     </div>
   );
 }
---
+++ components/ConceptItem.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ConceptItem({ name, title, description, image, link }) {
+  return (
+    <a href={link} target="_blank">
+      <div className="concept">
+        <div>
+          <Image src={image} alt={title} width={300} height={300} />
+        </div>
+        <div className="info">
+          <h2 className="title">{title}</h2>
+          <p className="author">{name}</p>
+          <p className="description">{description}</p>
+        </div>
+      </div>
+    </a>
+  );
+}
