48deb262557388d7dcee5f27109382f3b27eed6a
Andres Hung <andres.hung@outlook.com>
2023-10-19 13:28:00 -0600

    feat: created data objects file

---
+++ utils/data.js
@@ -0,0 +1,12 @@
+const concepts = [
+  {
+    name: "Andres Hung",
+    title: "Semaphores",
+    description:
+    "Semaphores are probably implemented flags that enforce mutual exclusion in critical regions.",
+    image: "/concept/ah-semaphores.png",
+    link: 'https://docs.google.com/presentation/d/1T07PFXmvSNUeJN7coH5SmyqNuF5qZQJJp8kjrfHIims/edit?usp=share_link',
+  },
+];
+
+export {concepts};
