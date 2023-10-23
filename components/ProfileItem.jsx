c0967ec0accf9cef2a64c26346cd13013d412013
Andres Hung <andres.hung@outlook.com>
2023-10-23 09:26:41 -0600

    feat: added to profiles and data

--- components/ProfileItem.jsx
+++ components/ProfileItem.jsx
@@ -1,17 +1,29 @@
 import Image from "next/image";
 
-export default function ProfileItem({ name, biography, image, link }) {
+export default function ProfileItem({
+  name,
+  title,
+  biography,
+  image,
+  github,
+  project,
+}) {
   return (
-    <a href={link} target="_blank">
-      <div className="profile">
-        <div>
-          <Image src={image} alt={name} width={500} height={500} />
-        </div>
-        <div className="info">
-          <h2>{name}</h2>
-          <p>{biography}</p>
-        </div>
+    <div className="profile">
+      <div>
+        <Image src={image} alt={name} width={500} height={500} />
       </div>
-    </a>
+      <div className="info">
+        <h2>{name}</h2>
+        <div className="profile-title">{title}</div>
+        <p>{biography}</p>
+        <a href={github} target="_blank">
+          <div className="profile-link">GitHub</div>
+        </a>
+        <a href={project} target="_blank">
+          <div className="profile-link">FOC Website</div>
+        </a>
+      </div>
+    </div>
   );
 }
--- utils/data.js
+++ utils/data.js
@@ -3,14 +3,14 @@ const concepts = [
     name: "Andres Hung",
     title: "Semaphores",
     description:
-    "Semaphores are probably implemented flags that enforce mutual exclusion in critical regions.",
+    "Semaphores are properly implemented flags that enforce mutual exclusion in critical regions.",
     image: "/concept/ah-semaphores.png",
     link: 'https://docs.google.com/presentation/d/1T07PFXmvSNUeJN7coH5SmyqNuF5qZQJJp8kjrfHIims/edit?usp=share_link',
   },
   {
     name: "Jennessa Sierra",
     title: "Internet Applications",
-    description: "Internet Applications are applications that run on the internet.",
+    description: "Internet Applications are applications that built on the application layer of the internet protocol suite.",
     image: "/concept/js-internet-applications.png",
     link: "https://docs.google.com/presentation/d/10RCNFoBkhy49K3KyMioW_9bEd2450ayQM8dH3pMtFt0/edit?usp=sharing"
   }
@@ -51,27 +51,35 @@ const projects = [
 const profiles = [
   {
     name: "Andres Hung",
+    title: "Part-time Kangaroo",
     biography: "I enjoy working with computers and learning about the technologies used to power the web today! I am currently working on my Associates's Degree in Information Technology at the University of Belize.",
     image: "https://avatars.githubusercontent.com/u/115679125?v=4",
-    link: "https://github.com/andreshungbz",
+    github: "https://github.com/andreshungbz",
+    project: "https://github.com/andreshungbz"
   },
   {
     name: "Jennessa Sierra",
-    biography: "Placeholder",
+    title: "Title",
+    biography: "Biography",
     image: "https://avatars.githubusercontent.com/u/143227481?v=4",
-    link: "https://github.com/jennxsierra",
+    github: "https://github.com/jennxsierra",
+    project: "https://github.com/jennxsierra"
   },
   {
-    name: "Placeholder",
-    biography: "Placeholder",
+    name: "Tysha Daniels",
+    title: "Title",
+    biography: "Biography",
     image: "/profile/warwick.png",
-    link: "https://github.com/andreshungbz",
+    github: "https://github.com/andreshungbz",
+    project: "https://github.com/andreshungbz"
   },
   {
-    name: "Placeholder",
-    biography: "Placeholder",
+    name: "Name",
+    title: "Title",
+    biography: "Biography",
     image: "/profile/warwick.png",
-    link: "https://github.com/andreshungbz",
+    github: "https://github.com/andreshungbz",
+    project: "https://github.com/andreshungbz"
   }
 ];
 
