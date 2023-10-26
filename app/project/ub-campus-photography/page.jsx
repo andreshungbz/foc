6a8f0b0b1d9243af76c8b5bb8f0c94ee2a5ad30c
Andres Hung <andres.hung@outlook.com>
2023-10-26 17:36:28 -0600

    feat: linked external project

--- app/project/ub-campus-photography/page.jsx
+++ app/project/ub-campus-photography/page.jsx
@@ -13,6 +13,20 @@ export default function UBCampusPhotographyPage() {
       <Link href="/project">
         <div className="backlink">⬅️ Back to Projects</div>
       </Link>
+      <div className="mt-3 border-2 border-indigo-600 bg-indigo-50 p-3">
+        <p className="m-0">
+          This is my personal project that I did for CS50. You can find the
+          project{" "}
+          <a
+            href="https://ub-campus-photography.vercel.app/"
+            target="_blank"
+            className="text-indigo-600 underline hover:text-indigo-800"
+          >
+            here
+          </a>
+          !
+        </p>
+      </div>
     </div>
   );
 }
