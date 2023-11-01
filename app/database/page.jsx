de1f09927c3e9a900330a2b64fe5e85fa412e860
Andres Hung <andres.hung@outlook.com>
2023-11-01 11:44:17 -0600

    feat: completed Database and Documentation pages

--- app/database/page.jsx
+++ app/database/page.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export const metadata = {
   title: "Database",
   description:
@@ -6,8 +8,35 @@ export const metadata = {
 
 export default function Database() {
   return (
-    <div>
-      <h1>Database</h1>
-    </div>
+    <a
+      href="https://drive.google.com/file/d/1gL6zYcdEBFgzvh-4iPgwfkvESouDlsbX/view?usp=drive_link"
+      target="_blank"
+    >
+      <div
+        className="flex flex-col items-center gap-3 border-2 border-lime-600 bg-lime-100 p-4 
+    transition-all hover:-translate-y-1 hover:translate-x-1 hover:bg-lime-200"
+      >
+        <div>
+          <Image
+            src="/database-screen.png"
+            alt="Database demonstration"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="mx-auto block h-auto w-5/6 border-2 border-neutral-500 shadow-lg"
+          />
+        </div>
+        <div className="p-3 text-center">
+          <h2 className="text-3xl font-bold uppercase text-lime-800">
+            Database Demonstration
+          </h2>
+          <p className="text-center text-emerald-600">by Dr. Ryan</p>
+          <p className="mb-0 mt-3 text-center">
+            A database is a collection of data that is multidimensional. Click
+            to view a video demonstration of databases.
+          </p>
+        </div>
+      </div>
+    </a>
   );
 }
--- app/documentation/page.jsx
+++ app/documentation/page.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export const metadata = {
   title: "Documentation",
   description:
@@ -6,8 +8,35 @@ export const metadata = {
 
 export default function Documentation() {
   return (
-    <div>
-      <h1>Documentation</h1>
-    </div>
+    <a
+      href="https://drive.google.com/file/d/1gL6zYcdEBFgzvh-4iPgwfkvESouDlsbX/view?usp=drive_link"
+      target="_blank"
+    >
+      <div
+        className="flex flex-col items-center gap-3 border-2 border-lime-600 bg-lime-100 p-4 
+    transition-all hover:-translate-y-1 hover:translate-x-1 hover:bg-lime-200"
+      >
+        <div>
+          <Image
+            src="/documentation-screen.png"
+            alt="Documentation demonstration"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="mx-auto block h-auto w-5/6 border-2 border-neutral-500 shadow-lg"
+          />
+        </div>
+        <div className="p-3 text-center">
+          <h2 className="text-3xl font-bold uppercase text-lime-800">
+            Documentation Example
+          </h2>
+          <p className="text-center text-emerald-600">by Jacob Petkau</p>
+          <p className="mb-0 mt-3 text-center">
+            Documentation is an important part of software development. Click to
+            view an example for the proposed EDUDatabase project.
+          </p>
+        </div>
+      </div>
+    </a>
   );
 }
